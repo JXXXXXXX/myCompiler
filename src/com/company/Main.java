58f1ab1ce83b06952939d3500041918932d136cb
Jin <jinxin19980410@163.com>
2019-04-25 13:53:52 +0800

    完成Parser.get_grammar()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,12 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //Lexer lexer = new Lexer();
-        Parser p=new Parser();
-        String  str = "123int";
-        System.out.println(p.getNextSymbol(str,3));
+        Lexer lexer = new Lexer();
+
     }
 }
--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -1,17 +1,16 @@
 package com.company;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Vector;
+import java.util.*;
 
 public class Parser {
     private String file_buffer;
     public Vector<Grammar> G;
     public Vector<String> V,T;
     public HashMap<String, HashSet<String>> FIRST,FOLLOW;
+    public HashMap<String,HashSet<Integer>>indexToV;// HashSet是非终结符号的定义式（有若干个）
     public static String[]  _V={"P","D","S","L","E","C","T","F"},
-                            _T={"id","int","float","if","else","while","num", ";",">","<","==","=","+","-","*","/","(",")","ε"};
+                            _T={"id","int","float","if","else","while","num", ";",">","<","==","=","+","-","*","/","(",")","~"};
 
     public void readfile(String filepath){
         // 读源程序文件
@@ -21,7 +20,7 @@ public class Parser {
             File file = new File(filepath);
             Reader reader = new InputStreamReader(new FileInputStream(file));
             while((temp_char=(char) reader.read())!='$'){
-                if (temp_char==' ')
+                if (temp_char==' '||temp_char=='\t'||temp_char=='\r')
                     continue;
                 else {
                     file_buffer = file_buffer + temp_char;
@@ -34,7 +33,7 @@ public class Parser {
             System.exit(0);
         }
     }
-    public String  getNextSymbol(String str,int index){
+    public String getNextSymbol(String str,int index){
         // 在字符串str里的index处向后，找第一个匹配的文法V或T
         while(str.charAt(index)==' '){
             index++;
@@ -51,6 +50,8 @@ public class Parser {
             if(str.indexOf(T.get(i),index)==index)
                 nextSymbol = T.get(i);
         }
+        if(str.charAt(index)=='-'&&str.charAt(index+1)=='>')
+            nextSymbol="->";
         return nextSymbol;
     }
 
@@ -66,8 +67,6 @@ public class Parser {
         // 2. 读文件
         String filepath = "E://Grammar.txt";
         readfile(filepath);
-        for(int i=0;i<file_buffer.length();i++)
-            System.out.println(file_buffer.charAt(i)-'\0');
         // 3.增广文法
         Grammar g0 = new Grammar();
         g0.left = "START";
@@ -75,32 +74,64 @@ public class Parser {
         G.add(g0);      // 添加到文法G
         V.add("START"); // 添加到非终结符V
 
-        for(int index=0;file_buffer.charAt(index)!='\0';index+=2)
+        int row=0; // 记录经历的行数
+        for(int index=0;file_buffer.charAt(index)!='\0'; )
         {
             String left = null;
             String right = null;
             Grammar new_grammar = new Grammar();
-            System.out.println("left:");
-            while(file_buffer.charAt(index)!='-' && file_buffer.charAt(index+1)!='>'){
+            while((left=getNextSymbol(file_buffer,index))!="->"){
                 // left
                 left = getNextSymbol(file_buffer,index);
-                System.out.println(left);
-                index=index+left.length();
+                if (left!=null){
+                    new_grammar.left = left;
+                    index=index+left.length();
+                }
+                else {
+                    System.out.println("文法符号查找错误"+",left"+",row:"+row);
+                    System.exit(0);
+                }
             }
+            index+=left.length();
 
-            new_grammar.left = left;
-            index=index+2; // 跳过 ‘->’
-            System.out.println("right:");
-            while(file_buffer.charAt(index)!='\0'){
+            while(file_buffer.charAt(index)!='\n'){
                 // right
                 right = getNextSymbol(file_buffer,index);
-                System.out.println(right);
-                new_grammar.right.add(right);
-                index+=right.length();
-                // TODO 空指针
+                if(right!=null){
+                    new_grammar.right.add(right);
+                    index+=right.length();
+                }
+                else {
+                    System.out.println("文法符号查找错误"+",right"+",row:"+row);
+                    System.exit(0);
+                }
             }
 
+            G.add(new_grammar);
+            index+=1;
+            row++;
         }
+
+        // 4.生成indexToV
+        for(int i=0;i<G.size();i++){
+            String left = G.get(i).left;
+            if(indexToV.containsKey(left)){ // 若HashMap包含了left这个非终结符
+                indexToV.get(left).add(i);  // 添加索引
+            }
+            else {
+                indexToV.put(left,new HashSet<Integer>());// 创建关于该非终结符的map键
+                indexToV.get(left).add(i);// 添加索引
+            }
+        }
+    }
+
+    public boolean inVT(String s){
+        // 判断s在集合V还是T中，若在V中，返回True
+        for(int i=0;i<V.size();i++){
+            if(s.equals(V.get(i)))
+                return true;
+        }
+        return false;
     }
     public void get_first(){
 
@@ -112,6 +143,9 @@ public class Parser {
         G = new Vector<Grammar>();  // 文法
         V = new Vector<String>();   // 非终结符
         T = new Vector<String>();   // 终结符
+        FIRST = new HashMap<>();
+        FOLLOW = new HashMap<>();
+        indexToV = new HashMap<>();
 
         get_garmmer();  //从文件读入文法符号
         get_first();    //获得FIRST集
